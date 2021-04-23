c084324ec903f6f8f588dbe05ea77a6b20968eec
Deepti Singh <68264994+Deeptisingh29-tech@users.noreply.github.com>
2021-04-23 11:30:27 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    <p style="color:white; margin:10px">
+                    Finding a job shouldn’t be a full-time job. Tell us what you’re looking for and we’ll get to work for you.
+Get Started
+    </p>
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+            
+    <!-- <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>         -->
+   
+        </div>
+        </section>
